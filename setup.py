97e578761817d178c643c474292d33dbfd96739e
Henry Rizzi <rizzi.henry@gmail.com>
2020-01-16 10:40:23 -0600

    move higher semvar since backwards compatibility breaking change but still not 'stable'

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ install_requires = ['psycopg2-binary>=2.7.1',
 
 setup(
     name='luigi_postgres_dburl',
-    version='0.0.3',
+    version='0.1.0',
     package_dir={'luigi_postgres_dburl': 'luigi_postgres_dburl'},
     packages=find_packages(),
     install_requires=install_requires,
